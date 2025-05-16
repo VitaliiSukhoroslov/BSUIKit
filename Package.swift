e11721b1ade8aa466bb8a20cfb7216ffa266addd
Сухорослов Виталий <v.sukhoroslov@l-tech.ru>
2025-05-16 13:50:37 +0300

    Initial commit

--- Package.swift
+++ Package.swift
@@ -1,21 +1,33 @@
-// swift-tools-version: 6.1
+// swift-tools-version: 5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "BSUIKit",
+    platforms: [
+        .iOS(.v15)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "BSUIKit",
             targets: ["BSUIKit"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/SnapKit/SnapKit.git", from: "5.7.1"),
+        .package(url: "https://github.com/onevcat/Kingfisher.git", from: "8.3.2")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "BSUIKit"),
+            name: "BSUIKit",
+            dependencies: [
+                .product(name: "SnapKit", package: "SnapKit"),
+                .product(name: "Kingfisher", package: "Kingfisher")
+            ]
+        ),
         .testTarget(
             name: "BSUIKitTests",
             dependencies: ["BSUIKit"]
