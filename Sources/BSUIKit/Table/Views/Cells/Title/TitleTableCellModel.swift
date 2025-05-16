73ef6a563797ded81162efb1a7d7f1214863d5a8
Сухорослов Виталий <v.sukhoroslov@l-tech.ru>
2025-05-16 18:32:17 +0300

    - change property

--- Sources/BSUIKit/Table/Views/Cells/Title/TitleTableCellModel.swift
+++ Sources/BSUIKit/Table/Views/Cells/Title/TitleTableCellModel.swift
@@ -38,13 +38,15 @@ open class TitleTableCellModel: BSTableViewCompatible {
         return cell
     }
 
-    open var labelSettings: LabelSettings = .init(
-        alignment: .left,
-        font: .systemFont(ofSize: 17, weight: .semibold),
-        textColor: .black,
-        numberOfLines: 0,
-        insets: .init(top: 16, left: 16, bottom: 16, right: 16)
-    )
+    open var labelSettings: LabelSettings {
+        .init(
+            alignment: .left,
+            font: .systemFont(ofSize: 17, weight: .semibold),
+            textColor: .black,
+            numberOfLines: 0,
+            insets: .init(top: 16, left: 16, bottom: 16, right: 16)
+        )
+    }
 }
 
 extension TitleTableCellModel {
